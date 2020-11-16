c1db71c2f453efe3c8a59b8eba0dd7f9909b932a
Ales Hotko <ales.hotko@tompit.net>
2020-11-16 16:01:30 +0100

    Update of Common driver and README.md

--- src/Driver.MathExtended.Common/Program.cs
+++ src/Driver.MathExtended.Common/Program.cs
@@ -1,27 +1,20 @@
 ﻿using MathExtended.Common;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Driver.MathExtended.Common
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void ClampingTest()
         {
-
-
-            Console.OutputEncoding = Encoding.UTF8;
-
-            Console.CursorVisible = false;
-
+            Console.WriteLine($"Clamping Functions");
+            Console.WriteLine();
             Console.WriteLine($"    x   default    limits    SmoothStep3    SmoothStep5    SmoothStep7    SmterStep");
             Console.WriteLine($"===================================================================================");
 
             //clamping
-            for (double x = -5.0; x<5.0; x += 0.1)
+            for (double x = -5.0; x < 5.0; x += 0.1)
             {
                 double clampDefault = Clamping.Clamp(x);
                 double clampLimited = Clamping.Clamp(x, -2.0, 1.5);
@@ -57,8 +50,56 @@ namespace Driver.MathExtended.Common
                 Console.WriteLine($"Clamping.SmoothStep7({x:N1}) = {smoothStep7:N3}");
                 Console.WriteLine($"Clamping.SmootherStep({x:N1}) = {smootherStep:N3}");
             }
+        }
 
+        private static void MappingTest()
+        {
+            Console.WriteLine($"Mapping Functions");
             Console.WriteLine();
+            Console.WriteLine($"    x    mapped");
+            Console.WriteLine($"===================================================================================");
+
+            for (double x = -5.0; x <= 5.0; x += 0.5)
+            {
+                double mapping1 = Mapping.Map(x, -5.0, 5.0, 3.0, 8.0);
+
+                Console.WriteLine($"{x,5:N1} {mapping1,9:N1}");
+            }
+
+            Console.WriteLine($"===================================================================================");
+        }
+
+        private static void WrappingTest()
+        {
+            Console.WriteLine($"Wrapping Functions");
+            Console.WriteLine();
+            Console.WriteLine($"    x    wrapped");
+            Console.WriteLine($"===================================================================================");
+
+            for (double x = -5.0; x <= 5.0; x += 0.5)
+            {
+                double mapping1 = Wrapping.Wrap(x, -1, 1);
+
+                Console.WriteLine($"{x,5:N1} {mapping1,9:N1}");
+            }
+
+            Console.WriteLine($"===================================================================================");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+
+            ClampingTest();
+            Console.WriteLine();
+
+            MappingTest();
+            Console.WriteLine();
+
+            WrappingTest();
+            Console.WriteLine();
+
             Console.ReadKey();
         }
     }
